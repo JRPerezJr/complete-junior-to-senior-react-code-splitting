1201e529be3cceadab10518fa54a717820f2872c
Juan <pront23@gmail.com>
2021-06-02 14:13:19 +0900

    added route state and functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to React</h1>
-      </header>
-      <Page1 />
-      <Page2 />
-      <Page3 />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1',
+    };
+  }
+
+  onRouteChange = route => this.setState({ route: route });
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Welcome to React</h1>
+        </header>
+        <Page1 onRouteChange={this.onRouteChange} />
+        <Page2 onRouteChange={this.onRouteChange} />
+        <Page3 onRouteChange={this.onRouteChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Page3.js
+++ src/components/Page3.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from '../logo.svg';
 
-const Page3 = () => (
+const Page3 = ({ onRouteChange }) => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1>Welcome to Page3</h1>
     </header>
-    <button onClick={() => console.log('clicked')}>Page1</button>
-    <button onClick={() => console.log('clicked')}>Page2</button>
+    <button onClick={() => onRouteChange('page1')}>Page1</button>
+    <button onClick={() => onRouteChange('page2')}>Page2</button>
   </div>
 );
 export default Page3;
