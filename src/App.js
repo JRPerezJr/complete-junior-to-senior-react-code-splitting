bfd3ea1de0d0d8658e6db147df6ae0629dcf89f9
Juan <pront23@gmail.com>
2021-06-02 14:19:52 +0900

    added route logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
@@ -16,17 +15,13 @@ class App extends Component {
   onRouteChange = route => this.setState({ route: route });
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to React</h1>
-        </header>
-        <Page1 onRouteChange={this.onRouteChange} />
-        <Page2 onRouteChange={this.onRouteChange} />
-        <Page3 onRouteChange={this.onRouteChange} />
-      </div>
-    );
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page2') {
+      return <Page2 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page3') {
+      return <Page3 onRouteChange={this.onRouteChange} />;
+    }
   }
 }
 
