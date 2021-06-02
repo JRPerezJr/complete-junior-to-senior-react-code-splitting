7a0bfc71d56ee92bc74ba96765d4bfbb9d0b89e4
Juan <pront23@gmail.com>
2021-06-02 14:52:55 +0900

    cleaned up unused code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-// import Page2 from './components/Page2';
-// import Page3 from './components/Page3';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       route: 'page1',
-      component: '',
+      component: null,
     };
   }
 
   onRouteChange = route => {
-    // no code splitting
-    // this.setState({ route: route });
     // with code splitting
     if (route === 'page1') {
       this.setState({ route: route });
@@ -31,15 +27,6 @@ class App extends Component {
   };
 
   render() {
-    // without code splitting
-    // if (this.state.route === 'page1') {
-    //   return <Page1 onRouteChange={this.onRouteChange} />;
-    // } else if (this.state.route === 'page2') {
-    //   return <Page2 onRouteChange={this.onRouteChange} />;
-    // } else if (this.state.route === 'page3') {
-    //   return <Page3 onRouteChange={this.onRouteChange} />;
-    // }
-
     // with code splitting
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />;
